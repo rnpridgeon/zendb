c785ce19b04ff5f25564ec837b9fcf767e5cbba8
rnpridgeon <ryan.n.pridgeon@gmail.com>
2018-05-03 11:08:58 -0400

    sed password incase of overwrite

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,2 +1,9 @@
-sudo -E git -C $GOPATH/src/github.com/rnpridgeon/zendb pull
-sudo -E go install github.com/rnpridgeon/zendb
+CONF=/etc/zendb/zendb.json
+
+if [ "$#" -eq 1 ]; then
+        CONF=$1
+fi
+
+git -C $GOPATH/src/github.com/rnpridgeon/zendb pull
+go install github.com/rnpridgeon/zendb
+sed -i s/'password'/$(cat $CONF | jq .database.password | sed s/\"//g)/ $GOPATH/src/github.com/rnpridgeon/zendb/scripts/mysql/mysql.sql
